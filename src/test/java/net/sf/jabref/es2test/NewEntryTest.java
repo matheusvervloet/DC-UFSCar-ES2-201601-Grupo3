2da6e8d0057a11e525890689636ba555d4c143d1
Matheus V <matheusvervloet@gmail.com>
2016-05-13 17:27:20 -0300

    Adicinado um teste simples.

--- src/test/java/net/sf/jabref/es2test/NewEntryTest.java
+++ src/test/java/net/sf/jabref/es2test/NewEntryTest.java
@@ -2,8 +2,23 @@ package net.sf.jabref.es2test;
 
 import net.sf.jabref.model.entry.BibEntry;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NewEntryTest {
 
     private BibEntry newEntry;
+
+    @Before
+    public void setUp() {
+        newEntry = new BibEntry();
+    }
+
+    @Test
+    public void basicTest() {
+        newEntry.setField("year", "2003");
+        Assert.assertEquals("2003", newEntry.getFieldOrAlias("year"));
+    }
 
 }
