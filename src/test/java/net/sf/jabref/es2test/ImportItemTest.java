709ac2897c91c53ce6efcdca034d77bea2a0bcda
Matheus V <matheusvervloet@gmail.com>
2016-06-03 15:42:20 -0300

    Criado arquivo esqueleto para testar importacao de bibtex.

---
+++ src/test/java/net/sf/jabref/es2test/ImportItemTest.java
@@ -0,0 +1,18 @@
+package net.sf.jabref.es2test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImportItemTest {
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void basicTest() {
+    }
+
+
+}
