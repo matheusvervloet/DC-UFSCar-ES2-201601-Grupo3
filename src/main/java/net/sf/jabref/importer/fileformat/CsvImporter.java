844544820e91e00b8a57917b4736a3ea0ccba6ae
Matheus V <matheusvervloet@gmail.com>
2016-06-23 03:45:46 -0300

    finalizada importação de csv

--- src/main/java/net/sf/jabref/importer/fileformat/CsvImporter.java
+++ src/main/java/net/sf/jabref/importer/fileformat/CsvImporter.java
@@ -29,7 +29,6 @@ import net.sf.jabref.importer.ImportFormatReader;
 import net.sf.jabref.importer.OutputPrinter;
 import net.sf.jabref.model.entry.AuthorList;
 import net.sf.jabref.model.entry.BibEntry;
-import net.sf.jabref.model.entry.MonthUtil;
 
 /**
  * Imports a Biblioscape Tag File. The format is described on
@@ -39,7 +38,7 @@ import net.sf.jabref.model.entry.MonthUtil;
  */
 public class CsvImporter extends ImportFormat {
 
-    private static final Pattern RECOGNIZED_FORMAT_PATTERN = Pattern.compile("[^,\n]*\n?(,[^,\n]*\n?)*");
+    private static final Pattern RECOGNIZED_FORMAT_PATTERN = Pattern.compile("[^,\n]*(,[^,\n]*)*");
 
     /**
      * Return the name of this import format.
@@ -93,163 +92,110 @@ public class CsvImporter extends ImportFormat {
             }
         }
 
-        String[] entries = sb.toString().replace("\u2013", "-").replace("\u2014", "--").replace("\u2015", "--")
-                .split("ER  -.*\\n");
+        String[] entries = sb.toString().split("\n");
 
         for (String entry1 : entries) {
 
             String type = "";
             String author = "";
             String editor = "";
-            String startPage = "";
-            String endPage = "";
             String comment = "";
             Map<String, String> hm = new HashMap<>();
 
-            String[] fields = entry1.split("\n");
+            String[] fields = entry1.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+            type = fields[0];
 
-            for (int j = 0; j < fields.length; j++) {
-                StringBuilder current = new StringBuilder(fields[j]);
-                boolean done = false;
-                while (!done && (j < (fields.length - 1))) {
-                    if ((fields[j + 1].length() >= 6) && !"  - ".equals(fields[j + 1].substring(2, 6))) {
-                        if ((current.length() > 0)
-                                && !Character.isWhitespace(current.charAt(current.length() - 1))
-                                && !Character.isWhitespace(fields[j + 1].charAt(0))) {
-                            current.append(' ');
-                        }
-                        current.append(fields[j + 1]);
-                        j++;
-                    } else {
-                        done = true;
-                    }
+            String bibtexkey = fields[1];
+
+            for (int j = 2; j < (fields.length - 1); j += 2) {
+                String lab = fields[j];
+                String val = fields[j + 1];
+
+                if (val.endsWith("\"") && val.startsWith("\"")) {
+                    val = val.substring(1, val.length() - 1);
                 }
-                String entry = current.toString();
-                if (entry.length() < 6) {
-                    continue;
-                } else {
-                    String lab = entry.substring(0, 2);
-                    String val = entry.substring(6).trim();
-                    if ("TY".equals(lab)) {
-                        if ("BOOK".equals(val)) {
-                            type = "book";
-                        } else if ("JOUR".equals(val) || "MGZN".equals(val)) {
-                            type = "article";
-                        } else if ("THES".equals(val)) {
-                            type = "phdthesis";
-                        } else if ("UNPB".equals(val)) {
-                            type = "unpublished";
-                        } else if ("RPRT".equals(val)) {
-                            type = "techreport";
-                        } else if ("CONF".equals(val)) {
-                            type = "inproceedings";
-                        } else if ("CHAP".equals(val)) {
-                            type = "incollection";//"inbook";
-                        } else {
-                            type = "other";
-                        }
-                    } else if ("T1".equals(lab) || "TI".equals(lab)) {
-                        String oldVal = hm.get("title");
-                        if (oldVal == null) {
-                            hm.put("title", val);
-                        } else {
-                            if (oldVal.endsWith(":") || oldVal.endsWith(".") || oldVal.endsWith("?")) {
-                                hm.put("title", oldVal + " " + val);
-                            } else {
-                                hm.put("title", oldVal + ": " + val);
-                            }
-                        }
-                        hm.put("title", hm.get("title").replaceAll("\\s+", " ")); // Normalize whitespaces
-                    } else if ("T2".equals(lab) || "BT".equals(lab)) {
-                        hm.put("booktitle", val);
-                    } else if ("T3".equals(lab)) {
-                        hm.put("series", val);
-                    } else if ("AU".equals(lab) || "A1".equals(lab)) {
-                        if ("".equals(author)) {
-                            author = val;
-                        } else {
-                            author += " and " + val;
-                        }
-                    } else if ("A2".equals(lab)) {
-                        if ("".equals(editor)) {
-                            editor = val;
-                        } else {
-                            editor += " and " + val;
-                        }
-                    } else if ("JA".equals(lab) || "JF".equals(lab) || "JO".equals(lab)) {
-                        if ("inproceedings".equals(type)) {
-                            hm.put("booktitle", val);
-                        } else {
-                            hm.put("journal", val);
-                        }
-                    } else if ("SP".equals(lab)) {
-                        startPage = val;
-                    } else if ("PB".equals(lab)) {
-                        if ("phdthesis".equals(type)) {
-                            hm.put("school", val);
-                        } else {
-                            hm.put("publisher", val);
-                        }
-                    } else if ("AD".equals(lab) || "CY".equals(lab)) {
-                        hm.put("address", val);
-                    } else if ("EP".equals(lab)) {
-                        endPage = val;
-                        if (!endPage.isEmpty()) {
-                            endPage = "--" + endPage;
-                        }
-                    } else if ("SN".equals(lab)) {
-                        hm.put("issn", val);
-                    } else if ("VL".equals(lab)) {
-                        hm.put("volume", val);
-                    } else if ("IS".equals(lab)) {
-                        hm.put("number", val);
-                    } else if ("N2".equals(lab) || "AB".equals(lab)) {
-                        String oldAb = hm.get("abstract");
-                        if (oldAb == null) {
-                            hm.put("abstract", val);
-                        } else {
-                            hm.put("abstract", oldAb + Globals.NEWLINE + val);
-                        }
-                    } else if ("UR".equals(lab)) {
-                        hm.put("url", val);
-                    } else if (("Y1".equals(lab) || "PY".equals(lab)) && (val.length() >= 4)) {
-                        String[] parts = val.split("/");
-                        hm.put("year", parts[0]);
-                        if ((parts.length > 1) && !parts[1].isEmpty()) {
-                            try {
 
-                                int monthNumber = Integer.parseInt(parts[1]);
-                                MonthUtil.Month month = MonthUtil.getMonthByNumber(monthNumber);
-                                if (month.isValid()) {
-                                    hm.put("month", month.bibtexFormat);
-                                }
-                            } catch (NumberFormatException ex) {
-                                // The month part is unparseable, so we ignore it.
-                            }
-                        }
-                    } else if ("KW".equals(lab)) {
-                        if (hm.containsKey("keywords")) {
-                            String kw = hm.get("keywords");
-                            hm.put("keywords", kw + ", " + val);
+                if ("title".equals(lab)) {
+                    String oldVal = hm.get("title");
+                    if (oldVal == null) {
+                        hm.put("title", val);
+                    } else {
+                        if (oldVal.endsWith(":") || oldVal.endsWith(".") || oldVal.endsWith("?")) {
+                            hm.put("title", oldVal + " " + val);
                         } else {
-                            hm.put("keywords", val);
-                        }
-                    } else if ("U1".equals(lab) || "U2".equals(lab) || "N1".equals(lab)) {
-                        if (!comment.isEmpty()) {
-                            comment = comment + " ";
-                        }
-                        comment = comment + val;
-                    }
-                    // Added ID import 2005.12.01, Morten Alver:
-                    else if ("ID".equals(lab)) {
-                        hm.put("refid", val);
-                    } else if ("M3".equals(lab)) {
-                        String doi = val;
-                        if (doi.startsWith("doi:")) {
-                            doi = doi.replaceAll("(?i)doi:", "").trim();
-                            hm.put("doi", doi);
+                            hm.put("title", oldVal + ": " + val);
                         }
                     }
+                    hm.put("title", hm.get("title").replaceAll("\\s+", " ")); // Normalize whitespaces
+                } else if ("booktitle".equals(lab)) {
+                    hm.put("booktitle", val);
+                } else if ("series".equals(lab)) {
+                    hm.put("series", val);
+                } else if ("author".equals(lab)) {
+                    if ("".equals(author)) {
+                        author = val;
+                    } else {
+                        author += " and " + val;
+                    }
+                } else if ("editor".equals(lab)) {
+                    if ("".equals(editor)) {
+                        editor = val;
+                    } else {
+                        editor += " and " + val;
+                    }
+                } else if ("booktitle".equals(lab) || "journal".equals(lab)) {
+                    if ("inproceedings".equals(type)) {
+                        hm.put("booktitle", val);
+                    } else {
+                        hm.put("journal", val);
+                    }
+                } else if ("pages".equals(lab)) {
+                    hm.put("pages", val);
+                } else if ("school".equals(lab) || "publisher".equals(lab)) {
+                    if ("phdthesis".equals(type)) {
+                        hm.put("school", val);
+                    } else {
+                        hm.put("publisher", val);
+                    }
+                } else if ("address".equals(lab)) {
+                    hm.put("address", val);
+                } else if ("issn".equals(lab)) {
+                    hm.put("issn", val);
+                } else if ("volume".equals(lab)) {
+                    hm.put("volume", val);
+                } else if ("number".equals(lab)) {
+                    hm.put("number", val);
+                } else if ("abstract".equals(lab)) {
+                    String oldAb = hm.get("abstract");
+                    if (oldAb == null) {
+                        hm.put("abstract", val);
+                    } else {
+                        hm.put("abstract", oldAb + Globals.NEWLINE + val);
+                    }
+                } else if ("url".equals(lab)) {
+                    hm.put("url", val);
+                } else if ("year".equals(lab)) {
+                    hm.put("year", val);
+                } else if ("keywords".equals(lab)) {
+                    if (hm.containsKey("keywords")) {
+                        String kw = hm.get("keywords");
+                        hm.put("keywords", kw + ", " + val);
+                    } else {
+                        hm.put("keywords", val);
+                    }
+                } else if ("comment".equals(lab)) {
+                    if (!comment.isEmpty()) {
+                        comment = comment + " ";
+                    }
+                    comment = comment + val;
+                } else if ("refid".equals(lab)) {
+                    hm.put("refid", val);
+                } else if ("doi".equals(lab)) {
+                    hm.put("doi", val);
+                } else if ("note".equals(lab)) {
+                    hm.put("note", val);
+                } else {
+                    hm.put(lab, val);
                 }
                 // fix authors
                 if (!author.isEmpty()) {
@@ -263,8 +209,6 @@ public class CsvImporter extends ImportFormat {
                 if (!comment.isEmpty()) {
                     hm.put("comment", comment);
                 }
-
-                hm.put("pages", startPage + endPage);
             }
             BibEntry b = new BibEntry(DEFAULT_BIBTEXENTRY_ID, type); // id assumes an existing database so don't
 
